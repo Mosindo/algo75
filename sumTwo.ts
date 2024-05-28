f3af0330d6a005bd27027516cb0ff4f25d4189cc
mosindo <mosindo.mbilinyama@hotmail.fr>
2024-05-28 17:24:51 +0200

    change

--- sumTwo.ts
+++ sumTwo.ts
@@ -7,7 +7,7 @@ function sumTwo(array: number[], taget: number): number[] | null {
   for (let i = 0; i < length; i++) {
     for (let j = i + 1; j < length; j++) {
       if (array[i] + array[j] === target) {
-        return [array[i], array[j]];
+        return [i, j];
       }
     }
   }
